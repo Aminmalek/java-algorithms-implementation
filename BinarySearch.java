d4ee67fe15f12d7903d6012960c0a16797355ba6
amin <aminmalek5@gmail.com>
2023-02-11 20:29:48 +0330

    add binary search

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch {
+    public static int binarySearch (int[] sortedArray,int key){
+        int lowerBound = 1;
+        int upperBound = sortedArray.length;
+        int keyPosition = -1 ;
+
+        while (keyPosition == -1){
+            if (upperBound < lowerBound){
+                System.out.println("x does not exists");
+                break;
+            }
+            int midpoint = lowerBound + ( upperBound - lowerBound ) / 2;
+            if (sortedArray[midpoint] < key) {
+                lowerBound = midpoint + 1;
+            }
+
+            if (sortedArray[midpoint] > key) {
+                lowerBound = midpoint - 1;
+            }
+
+            if (sortedArray[midpoint] == key) {
+                keyPosition = midpoint;
+                return keyPosition;
+            }
+
+        }
+        return 0;
+
+    }
+    public static void main(String[] args) {
+        int[] a1= {10,20,30,50,70,90};
+        int key = 50;
+        System.out.println(binarySearch( a1,key));
+    }
+}
---
+++ LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+    public  static int linearSearch(int[] array, int key) {
+        for(int i=0; i<array.length; i++){
+            if(array[i]==key) return i;
+        }
+    return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] a1= {10,20,30,50,70,90};
+        int key = 50;
+        System.out.println(linearSearch( a1,key));
+    }
+
+}
