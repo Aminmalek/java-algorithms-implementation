933e2b51a1e1fbb563f0d7330e7e861e65dd0fbb
aminmalek <aminmalek5@gmail.com>
2023-03-19 21:41:16 +0330

    merge sort initial

---
+++ algorithms/src/MergeSort.java
@@ -0,0 +1,48 @@
+public class MergeSort {
+
+    public static void mergeSort(int[] arr, int mid) {
+        var n = arr.length;
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = arr[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(arr, l, r, mid, n - mid);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            } else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 5, 1, 6, 2, 3, 4 };
+        mergeSort(arr, mid);
+    }
+}
